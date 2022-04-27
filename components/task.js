fb2536833b2e5393039f24721bdd0a1805653ed2
Ömer Faruk <sahan.faruk.omer@outlook.com>
2022-04-27 11:02:24 +0300

    to-do-list

---
+++ components/task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = () => {
+
+    return (
+        <View>
+            <Text>Bu bir mesajdır.</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Task;
